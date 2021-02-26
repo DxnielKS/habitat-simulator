346d46cbdf0a7d674132695c7404dd69428f66a8
daniel <47062269+DxnielKS@users.noreply.github.com>
2021-02-26 14:17:15 +0000

    Updated weather class

--- foxes-and-rabbits-handout/Weather.java
+++ foxes-and-rabbits-handout/Weather.java
@@ -9,26 +9,51 @@ import java.util.Random;
 public class Weather
 {
     // instance variables - replace the example below with your own
-    
+
+    private Random randomGenerator;
     private boolean clear, rain, storm, cloudy;
     private ArrayList<String> weather_list;
     /**
-     * Constructor for objects of class Weather
+     * Constructor for objects of class Weather. The weather defaults to clear weather.
      */
     public Weather()
     {
         // initialise instance variables
+        randomGenerator = new Random()
         weather_list = new ArrayList<String>();
         weather_list.add("Clear");
         weather_list.add("Rain");
         weather_list.add("Storm");
         weather_list.add("Cloudy");
-        clear = rain = storm = cloudy = false;
+        rain = storm = cloudy = false;
+        clear = true;
     }
-
+    /**
+     * Method to change the weather randomly in the simulator
+     */
     public void change_weather()
     {
-        return;
+        int new_weather_index = randomGenerator.nextInt(4)
+        if (weather_list.get(new_weather_index) == "Clear")
+        {
+            clear = true;
+            rain = storm = cloudy = false;
+        }
+        else if(weather_list.get(new_weather_index) == "Rain")
+        {
+            rain = true;
+            clear = storm = cloudy = false;
+        }
+        else if (weather_list.get(new_weather_index) == "Storm")
+        {
+            storm = true;
+            clear = rain = cloudy = false;
+        }
+        else if (weather_list.get(new_weather_index) == "Cloudy")
+        {
+            cloudy = true;
+            rain = storm = clear = false;
+        }
     }
 
     public String get_weather()
@@ -40,6 +65,16 @@ public class Weather
         else if (rain)
         {
             return "Rain";
+        }
+        else if (Storm)
+        {
+            return "Storm";
+        }
+        else if (Cloudy)
+        {
+            return "Cloudy";
+        }
+        }
         }
         return null;
     }
